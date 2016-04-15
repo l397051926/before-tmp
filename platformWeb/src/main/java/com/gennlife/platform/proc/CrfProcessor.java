5319989767d17a4d49bfa460f5d474c42a207558
chensong <chensong@gennlife.com>
2016-04-15 19:42:56 +0800

    crf数据录入功能,更新数据,bug fix

--- platformWeb/src/main/java/com/gennlife/platform/proc/CrfProcessor.java
+++ platformWeb/src/main/java/com/gennlife/platform/proc/CrfProcessor.java
@@ -7,10 +7,7 @@ import com.gennlife.platform.bean.crf.MongoResultBean;
 import com.gennlife.platform.bean.crf.SampleListBean;
 import com.gennlife.platform.bean.crf.SummaryBean;
 import com.gennlife.platform.dao.AllDao;
-import com.gennlife.platform.util.ChineseToEnglish;
-import com.gennlife.platform.util.GsonUtil;
-import com.gennlife.platform.util.MongoManager;
-import com.gennlife.platform.util.ParamUtils;
+import com.gennlife.platform.util.*;
 import com.gennlife.platform.view.View;
 import com.google.gson.*;
 import com.google.gson.internal.ObjectConstructor;
@@ -95,6 +92,7 @@ public class CrfProcessor {
                     summaryBean.setUsers(users);
                     summaryBean.setLastTime(t);
                     MongoManager.insertNewSummary(summaryBean);
+                    modelObj = ModelTreeUtils.updateUIData(modelObj);
                     data = gson.toJson(modelObj);
                     viewer.viewString(data, resps, request);
                     return;
@@ -107,6 +105,7 @@ public class CrfProcessor {
                 }
             } else {//该项目有模板,从数据库中获取的crf_id不空
                 JsonObject modelObj = MongoManager.getModel(summary.getCrf_id());
+                modelObj = ModelTreeUtils.updateUIData(modelObj);
                 data = gson.toJson(modelObj);
                 viewer.viewString(data, resps, request);
                 return;
@@ -115,6 +114,7 @@ public class CrfProcessor {
         } else {//前端请求的crf不空
             crf_id = paramObj.get("crf_id").getAsString();
             JsonObject modelObj = MongoManager.getModel(crf_id);
+            modelObj = ModelTreeUtils.updateUIData(modelObj);
             data = gson.toJson(modelObj);
             viewer.viewString(data, resps, request);
             return;
@@ -123,219 +123,7 @@ public class CrfProcessor {
 
     }
 
-    /**
-     * 递归遍历模型,将set<attrID>的属性为勾选状态,如果set为空,将全部属性勾选
-     *
-     * @param parent
-     * @param obj
-     * @param set
-     * @return
-     */
-    public static JsonObject traversalSelect(String parent, JsonObject obj, Set<String> set) {
-        if (obj == null || obj.isJsonNull()) {
-            return null;
-        }
-        if (obj.get("attrID") == null) {//组
-            String id = obj.get("id").getAsString();
-            JsonArray jsonArray = obj.getAsJsonArray("children");
-            for (int i = 0; i < jsonArray.size(); i++) {
-                JsonElement jsonObject = jsonArray.get(i);
-                traversalSelect(id, jsonObject.getAsJsonObject(), set);
-            }
-        } else {
-            if (set == null) {
-                obj.addProperty("checked", true);
-            } else {
-                String attrID = obj.get("attrID").getAsString();
-                if (set.contains(attrID)) {
-                    obj.addProperty("checked", true);
-                } else {
-                    if ("autoId".equals(attrID) || "name".equals(attrID)) {
-                        obj.addProperty("checked", true);
-                    } else {
-                        obj.addProperty("checked", false);
-                    }
-                }
-            }
-        }
-        return obj;
-    }
 
-    /**
-     * 递归遍历模型,将新定义组或者属性的信息放入响应的位置
-     *
-     * @param parent:父节点ID
-     * @param obj,当前的模型节点
-     * @param group,新的组,或者属性
-     * @return
-     */
-    public static JsonObject traversalAddGroup(String parent, JsonObject obj, JsonObject group) {
-        if (obj == null || obj.isJsonNull()) {
-            return null;
-        }
-        if (obj.get("attrID") == null) {//当前是组
-            String id = obj.get("id").getAsString();
-            JsonArray jsonArray = obj.getAsJsonArray("children");
-            if (id.equals(parent)) {//找到父节点
-                int rankID = jsonArray.size();
-                group.addProperty("rankID", rankID);
-                jsonArray.add(group);
-            } else {//
-                for (int i = 0; i < jsonArray.size(); i++) {
-                    JsonElement jsonObject = jsonArray.get(i);
-                    traversalAddGroup(parent, jsonObject.getAsJsonObject(), group);
-                }
-            }
-        } else {//当前是属性,什么也不做
-
-        }
-        return obj;
-    }
-
-    /**
-     * 递归查找,未被选中的属性,
-     *
-     * @param obj,当前节点
-     * @param set,保存选中的节点的路径
-     * @return
-     */
-    public static JsonObject traversalSelectAttr(String parent, JsonObject obj, Set<String> set) {
-        if (obj == null || obj.isJsonNull()) {
-            return null;
-        }
-        if (obj.get("attrID") == null) {//当前是组
-            String id = obj.get("id").getAsString();
-            JsonArray jsonArray = obj.getAsJsonArray("children");
-            for (int i = 0; i < jsonArray.size(); i++) {
-                JsonElement jsonObject = jsonArray.get(i);
-                traversalSelectAttr(id, jsonObject.getAsJsonObject(), set);
-            }
-        } else {//当前是属性
-            if (obj.get("checked") != null && obj.get("checked").getAsBoolean()) {
-                set.add(parent + "_" + obj.get("attrID").getAsString());
-            }
-        }
-        return obj;
-    }
-
-
-    /**
-     *深度优先遍历去除掉叶子节点
-     * @param parent
-     * @return
-     */
-    public static JsonObject traversalCutDownLeaves(JsonObject parent){
-        if(parent == null||parent.isJsonNull()){
-            return null;
-        }
-        if(parent.get("attrID") != null || parent.get("children") == null || parent.get("children").isJsonNull()){
-            return null;
-        }
-        JsonArray children = parent.get("children").getAsJsonArray();
-        JsonArray newChildren = new JsonArray();
-        for(JsonElement child:children){
-            JsonObject childObj = child.getAsJsonObject();
-            if(childObj.get("attrID") == null){//是组
-                JsonObject newchildObj = traversalCutDownLeaves(childObj);
-                if(newchildObj != null){
-                    newChildren.add(newchildObj);
-                }
-            }
-        }
-        if(newChildren.size() == 0){
-            parent.remove("children");
-        }else{
-            parent.add("children",newChildren);
-        }
-
-        return parent;
-    }
-
-    public static void traversalUpdateGroupName(JsonObject parent,String id,String newName){
-        if(parent.get("children") != null){//是组
-            String groupID = null;
-            if(parent.get("id") != null){//有children,有id,是组
-                groupID = parent.get("id").getAsString();
-                if(groupID.equals(id)){//找到组,重新命名
-                    parent.addProperty("name",newName);
-                    String newID = getNewGroupID(id,newName);
-                    parent.addProperty("id",newID);
-                }else{//当前不是要找组,遍历子节点
-                    if(groupID.startsWith(id)){//当前组是要找组的子组
-                        String newID = getNewGroupID(id,newName);
-                        groupID = groupID.replace(id,newID);
-                        parent.addProperty("id",groupID);
-                    }
-                    JsonArray children = parent.get("children").getAsJsonArray();
-                    for(JsonElement entity:children){
-                        JsonObject json = entity.getAsJsonObject();
-                        traversalUpdateGroupName(json,id,newName);
-                    }
-                }
-            }else {//有children 无id,是根节点
-                JsonArray children = parent.get("children").getAsJsonArray();
-                for(JsonElement entity:children){
-                    JsonObject json = entity.getAsJsonObject();
-                    traversalUpdateGroupName(json,id,newName);
-                }
-            }
-
-        }
-    }
-
-    public static void traversalDeleteGroup(JsonObject parent,String id,String parentID){
-        if(id.equals(parentID)){//删除的是最底层的组
-            JsonArray children = parent.getAsJsonArray("children");
-            JsonArray newchildren = new JsonArray();
-            for(JsonElement entity:children){
-                JsonObject obj = entity.getAsJsonObject();
-                if(obj.get("id") == null ||(obj.get("id") != null && !id.equals(obj.get("id").getAsString()))){
-                    newchildren.add(obj);
-                }
-            }
-            parent.add("children",newchildren);
-        }else{//删除不是最底层的组
-            if(parent.get("children") != null){
-                String groupID = parent.get("id") == null ? "Root":parent.get("id").getAsString();
-                JsonArray children = parent.getAsJsonArray("children");
-                if(groupID.equals(parentID)){
-                    JsonArray newchildren = new JsonArray();
-                    for(JsonElement entity:children){
-                        JsonObject obj = entity.getAsJsonObject();
-                        if(obj.get("id") == null || (obj.get("id") != null && !id.equals(obj.get("id").getAsString()))){
-                            newchildren.add(obj);
-                        }
-                    }
-                    parent.add("children",newchildren);
-                }else{
-                    for(JsonElement entity:children) {
-                        JsonObject obj = entity.getAsJsonObject();
-                        traversalDeleteGroup(obj,id,parentID);
-                    }
-                }
-            }
-        }
-
-    }
-
-
-    private static String getNewGroupID(String oldID,String newName){
-        String[] groups = oldID.split("_");
-        String groupID = "";
-        if(groups.length == 1){
-            groupID = newName;//新的组id
-        }else{
-            for(int i= 0;i <= groups.length -2;i++){
-                if(i == 0){
-                    groupID = groups[i];
-                }else{
-                    groupID = groupID + "_" + groups[i];
-                }
-            }
-            groupID = groupID + "_" + newName;
-        }
-        return groupID;
-    }
 
     /**
      * 生成默认的模板
@@ -406,7 +194,7 @@ public class CrfProcessor {
         JsonArray newDataObj = new JsonArray();
         List<BSONObject> list = new LinkedList<BSONObject>();
         for (JsonElement entity : dataObj) {
-            JsonObject obj = traversalSelect(null, entity.getAsJsonObject(), attrs);
+            JsonObject obj = ModelTreeUtils.traversalSelect(null, entity.getAsJsonObject(), attrs);
             newDataObj.add(obj);
             list.add(BasicDBObject.parse(gson.toJson(obj)));
         }
@@ -414,6 +202,7 @@ public class CrfProcessor {
         modelJsonObject.add("children", newDataObj);
         //将新的模型数据存入mongo
         MongoManager.updateNewModel(crf_id, list);
+        modelJsonObject = ModelTreeUtils.updateUIData(modelJsonObject);
         data = gson.toJson(modelJsonObject);
         viewer.viewString(data, resp, req);
     }
@@ -496,7 +285,7 @@ public class CrfProcessor {
             list.add(BasicDBObject.parse(gson.toJson(newGroup)));
         }else{
             for (JsonElement entity : dataObj) {
-                JsonObject obj = traversalAddGroup(parentsID, entity.getAsJsonObject(), newGroup);
+                JsonObject obj = ModelTreeUtils.traversalAddGroup(parentsID, entity.getAsJsonObject(), newGroup);
                 newDataObj.add(obj);
                 list.add(BasicDBObject.parse(gson.toJson(obj)));
             }
@@ -507,6 +296,8 @@ public class CrfProcessor {
         modelJsonObject.add("children", newDataObj);
         //将新的模型数据存入mongo
         MongoManager.updateNewModel(crf_id, list);
+        //
+        modelJsonObject = ModelTreeUtils.updateUIData(modelJsonObject);
         data = gson.toJson(modelJsonObject);
         viewer.viewString(data, resp, req);
     }
@@ -597,7 +388,7 @@ public class CrfProcessor {
         List<BSONObject> list = new LinkedList<BSONObject>();
         for (JsonElement entity : dataObj) {
             JsonObject obj = entity.getAsJsonObject();
-            obj = traversalAddGroup(groupID, obj, newAttr);
+            obj = ModelTreeUtils.traversalAddGroup(groupID, obj, newAttr);
             newDataObj.add(obj);
             list.add(BasicDBObject.parse(gson.toJson(obj)));
         }
@@ -605,6 +396,7 @@ public class CrfProcessor {
         modelJsonObject.add("children", newDataObj);
         //将新的模型数据存入mongo
         MongoManager.updateNewModel(crf_id, list);
+        modelJsonObject = ModelTreeUtils.updateUIData(modelJsonObject);
         data = gson.toJson(modelJsonObject);
         viewer.viewString(data, resp, req);
 
@@ -782,7 +574,7 @@ public class CrfProcessor {
             data.addProperty("crf_id", crf_id);
             data.addProperty("caseID", caseID);
             data.add("children", children);
-            String name = getUpdateForName(children);
+            String name = ModelTreeUtils.getUpdateForName(children);
             if(name != null){
                 data.addProperty("patientName", name);
             }
@@ -805,7 +597,7 @@ public class CrfProcessor {
 
         }else{//已经插入一部分,这部分是更新同名的组信息
             JsonArray jsonArr = existData.getAsJsonArray("children");
-            String name = getUpdateForName(jsonArr);
+            String name = ModelTreeUtils.getUpdateForName(children);
             JsonArray newChildren = new JsonArray();
             Map<String,JsonObject> map = new LinkedHashMap<String, JsonObject>();
             //现将原有的数据放入map
@@ -840,36 +632,7 @@ public class CrfProcessor {
         }
     }
 
-    /**
-     *
-     * @param children
-     * @return
-     */
-    public static String getUpdateForName(JsonArray children){
-        String name = null;
-        for (JsonElement entity : children) {
-            JsonObject obj = entity.getAsJsonObject();
-            if (obj.get("id") != null && "患者基本信息".equals(obj.get("id").getAsString())) {
-                if (obj.get("children").isJsonArray()) {
-                    JsonArray child = obj.getAsJsonArray("children");
-                    for (JsonElement en : child) {
-                        JsonObject object = en.getAsJsonObject();
-                        if (object.get("id") != null && "患者基本信息_基本信息".equals(object.get("id").getAsString())) {
-                            JsonArray child1 = object.getAsJsonArray("children");
-                            for (JsonElement ent : child1) {
-                                JsonObject object1 = ent.getAsJsonObject();
-                                if (object1.get("attrID") != null && "name".equals(object1.get("attrID").getAsString())) {
-                                    name = object1.get("data").getAsString();
-                                    return name;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return name;
-    }
+
 
     /**
      * crf数据录入完成后,点击录入完成出发的接口,请求参数crf_id,caseID,返回crf_id,新的caseID,空的数据格式
@@ -933,7 +696,7 @@ public class CrfProcessor {
             errorParam(err, req, resp);
             return;
         }
-        JsonObject modelTree = traversalCutDownLeaves(modelJsonObject);
+        JsonObject modelTree = ModelTreeUtils.traversalCutDownLeaves(modelJsonObject);
         viewer.viewString(gson.toJson(modelTree),resp,req);
     }
 
@@ -1063,7 +826,7 @@ public class CrfProcessor {
             errorParam(err, req, resp);
             return;
         }
-        traversalUpdateGroupName(modelJsonObject,groupID,groupName);
+        ModelTreeUtils.traversalUpdateGroupName(modelJsonObject,groupID,groupName);
         JsonArray children = modelJsonObject.get("children").getAsJsonArray();
         List<BSONObject> list = new LinkedList<BSONObject>();
         for(JsonElement ent:children){
@@ -1071,6 +834,7 @@ public class CrfProcessor {
             list.add(BasicDBObject.parse(gson.toJson(obj)));
         }
         MongoManager.updateNewModel(crf_id,list);
+        modelJsonObject = ModelTreeUtils.updateUIData(modelJsonObject);
         viewer.viewString(gson.toJson(modelJsonObject),resp,req);
     }
 
@@ -1095,7 +859,7 @@ public class CrfProcessor {
             return;
         }
         String parentID = getParentID(groupID);
-        traversalDeleteGroup(modelJsonObject,groupID,parentID);
+        ModelTreeUtils.traversalDeleteGroup(modelJsonObject,groupID,parentID);
         JsonArray children = modelJsonObject.get("children").getAsJsonArray();
         List<BSONObject> list = new LinkedList<BSONObject>();
         for(JsonElement ent:children){
@@ -1103,6 +867,7 @@ public class CrfProcessor {
             list.add(BasicDBObject.parse(gson.toJson(obj)));
         }
         MongoManager.updateNewModel(crf_id,list);
+        modelJsonObject = ModelTreeUtils.updateUIData(modelJsonObject);
         viewer.viewString(gson.toJson(modelJsonObject),resp,req);
     }
     private static String getParentID(String id){
