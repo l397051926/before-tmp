e4a77c830ec04760fc25827dd1d43c2329747f01
chensong <chensong@gennlife.com>
2016-06-13 17:31:41 +0800

    搜索病历 test

--- platformWeb/src/main/java/com/gennlife/platform/controller/CaseController.java
+++ platformWeb/src/main/java/com/gennlife/platform/controller/CaseController.java
@@ -22,22 +22,7 @@ public class CaseController {
     private Logger logger = LoggerFactory.getLogger(CaseController.class);
     private static JsonParser jsonParser = new JsonParser();
     private CaseProcessor processor = new CaseProcessor();
-    @RequestMapping(value="/SearchItemSet",method= RequestMethod.POST,produces = "application/json;charset=UTF-8")
-    public @ResponseBody
-    String postSearchItemSet(@RequestBody String param){
-        Long start = System.currentTimeMillis();
-        String resultStr = null;
-        try{
-            logger.info("搜索结果列表展示的集合 post方式 参数="+param);
-            JsonObject paramObj = (JsonObject) jsonParser.parse(param);
-            resultStr =  processor.searchItemSet(paramObj);
-        }catch (Exception e){
-            logger.error("搜索结果列表展示的集合",e);
-            resultStr = ParamUtils.errorParam("出现异常");
-        }
-        logger.info("搜索结果列表展示的集合 post 耗时"+(System.currentTimeMillis()-start) +"ms");
-        return resultStr;
-    }
+
     @RequestMapping(value="/SearchItemSet",method= RequestMethod.GET,produces = "application/json;charset=UTF-8")
     public @ResponseBody
     String getSearchItemSet(@RequestParam("param") String param){
@@ -52,6 +37,7 @@ public class CaseController {
             resultStr = ParamUtils.errorParam("出现异常");
         }
         logger.info("搜索结果列表展示的集合 get 耗时"+(System.currentTimeMillis()-start) +"ms");
+        logger.info("搜索结果列表展示的集合 结果="+resultStr);
         return resultStr;
     }
 
@@ -157,8 +143,7 @@ public class CaseController {
         return resultStr;
     }
     @RequestMapping(value="/SearchCase",method= RequestMethod.POST,produces = "application/json;charset=UTF-8")
-    @ResponseBody
-    public String postSearchCase(HttpServletRequest paramRe){
+    public @ResponseBody String postSearchCase(HttpServletRequest paramRe){
         Long start = System.currentTimeMillis();
         String resultStr = null;
         try{
@@ -171,6 +156,7 @@ public class CaseController {
             resultStr = ParamUtils.errorParam("出现异常");
         }
         logger.info("病历搜索 post 耗时"+(System.currentTimeMillis()-start) +"ms");
+        logger.info("病历搜索 结果="+resultStr);
         return resultStr;
     }
 
