d3352ae9775fbeee2269fb2876b018794c7f7721
chensong <chensong@gennlife.com>
2016-11-10 17:24:08 +0800

    添加管理员

--- platformWeb/src/main/java/com/gennlife/platform/filter/SessionFilter.java
+++ platformWeb/src/main/java/com/gennlife/platform/filter/SessionFilter.java
@@ -87,19 +87,20 @@ public class SessionFilter implements Filter {
                         if(user == null){
                             view.viewString(ParamUtils.errorParam("用户不存在"),response);
                         }else {
+                            if(adminSet.contains(uri)){
+                                logger.info("admin 判定"+gson.toJson(user));
+                                if(!AuthorityUtil.isAdmin(user)){//没有管理权限
+                                    view.viewString(ParamUtils.errorAuthorityParam(),response);
+                                }else{//放行
+                                    filterChain.doFilter(request,response);
+                                }
+                            }else {
+                                filterChain.doFilter(request,response);
+                            }
                             MemCachedUtil.setUserWithTime(uid,user, UserController.sessionTimeOut);
                         }
                     }
-                    if(adminSet.contains(uri)){
-                        logger.info("admin 判定"+gson.toJson(user));
-                        if(!AuthorityUtil.isAdmin(user)){//没有管理权限
-                            view.viewString(ParamUtils.errorAuthorityParam(),response);
-                        }else{//放行
-                            filterChain.doFilter(request,response);
-                        }
-                    }else {
-                        filterChain.doFilter(request,response);
-                    }
+
                 }catch (Exception e){
                     logger.error("",e);
                     view.viewString(ParamUtils.errorParam("session异常"),response);
