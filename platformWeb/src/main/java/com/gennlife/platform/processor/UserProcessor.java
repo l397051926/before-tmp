133b2cd2fcde528b03d5442e3a9ae702e05ab2b5
chensong <chensong@gennlife.com>
2016-11-23 17:03:12 +0800

    bug fix

--- platformWeb/src/main/java/com/gennlife/platform/processor/UserProcessor.java
+++ platformWeb/src/main/java/com/gennlife/platform/processor/UserProcessor.java
@@ -31,9 +31,6 @@ public class UserProcessor {
             User user = null;
             try{
                 user = AllDao.getInstance().getSyUserDao().getUser(confMap);
-                if(user != null){
-                    user.setPwd(null);//密码不返回
-                }
             }catch (Exception e){
                 logger.error("", e);
             }
@@ -146,7 +143,6 @@ public class UserProcessor {
         User user = null;
         try {
             user = AllDao.getInstance().getSyUserDao().getUserByUid(uid);
-            user.setPwd(null);
             Map<String,Object> confMap = new HashMap<>();
             confMap.put("orgID",user.getOrgID());
             confMap.put("uid",user.getUid());
