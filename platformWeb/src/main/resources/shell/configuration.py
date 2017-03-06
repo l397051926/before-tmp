59a84bbf4d0cf39421fbe15b376e33e9418906a2
luoxupan <ygxqqx@163.com>
2017-03-06 10:27:36 +0800

    case.json

--- platformWeb/src/main/resources/shell/configuration.py
+++ platformWeb/src/main/resources/shell/configuration.py
@@ -21,7 +21,7 @@ def sortItem(itemArray,allSortIndexName):
     return newArray
 
 def process():
-    data = xlrd.open_workbook('/Users/luoxupan/Downloads/病人维度临床数据字段配置_V0.2.1_beta.xls')
+    data = xlrd.open_workbook('/Users/luoxupan/Downloads/病人维度临床数据字段配置_V0.2.3_beta.xls')
     allItemList = OrderedDict()
     allItemListCopy = OrderedDict()
     sheet = data.sheets()[0]
