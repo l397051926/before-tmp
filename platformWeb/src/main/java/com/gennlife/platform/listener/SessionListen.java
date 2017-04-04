b8d642f5ce1b72275c244a139d06550bfdf0bd2d
chenjinfeng <chenjinfeng@gennlife.com>
2017-04-04 21:00:18 +0800

    bug fix

--- platformWeb/src/main/java/com/gennlife/platform/listener/SessionListen.java
+++ platformWeb/src/main/java/com/gennlife/platform/listener/SessionListen.java
@@ -15,13 +15,13 @@ public class SessionListen implements HttpSessionListener {
     private static final Logger logger= LoggerFactory.getLogger(SessionListen.class);
     @Override
     public void sessionCreated(HttpSessionEvent se) {
-        String sessionid=se.getSession(false).getId();
+        String sessionid=se.getSession().getId();
         logger.info("sessionid " +sessionid+" create");
     }
 
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
-        String sessionid=se.getSession(false).getId();
+        String sessionid=se.getSession().getId();
         logger.info("sessionid " +sessionid+" time out");
         RedisUtil.userLogout(sessionid);
         AllDao.getInstance().getSessionDao().deleteBySessionID(sessionid);
