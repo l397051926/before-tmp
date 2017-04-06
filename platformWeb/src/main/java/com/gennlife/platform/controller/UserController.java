f0d6dbc2fa7b22adc2b1411b2fba70c3ae7db2b4
luoxupan <ygxqqx@163.com>
2017-04-06 15:28:14 +0800

    merge

--- platformWeb/src/main/java/com/gennlife/platform/controller/UserController.java
+++ platformWeb/src/main/java/com/gennlife/platform/controller/UserController.java
@@ -40,7 +40,7 @@ public class UserController {
     private static View view = new View();
 
     @RequestMapping(value = "/Login", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
-    public void postLogin(HttpServletRequest paramRe, HttpServletResponse response){
+    public void postLogin(HttpServletRequest paramRe, HttpServletResponse response) {
         Long start = System.currentTimeMillis();
         String resultStr = null;
         try {
@@ -49,7 +49,7 @@ public class UserController {
             String email = null;
             String pwd = null;
             try {
-                JsonObject user = (JsonObject)jsonParser.parse(param);
+                JsonObject user = (JsonObject) jsonParser.parse(param);
                 email = user.get("email").getAsString();
                 pwd = user.get("pwd").getAsString();
             } catch (Exception e) {
@@ -61,21 +61,18 @@ public class UserController {
             if (user != null) {
                 HttpSession session = paramRe.getSession(true);
                 String sessionID = session.getId();
-                String loginSession= RedisUtil.getValue(user.getUid());
-                if (!StringUtils.isEmpty(loginSession)&&!loginSession.equals(sessionID)) {
-                    LogUtils.BussnissLog("用户 " + email + " 已经登陆在其他session,进行重新登陆 "+loginSession);
+                String loginSession = RedisUtil.getValue(user.getUid());
+                if (!StringUtils.isEmpty(loginSession) && !loginSession.equals(sessionID)) {
+                    LogUtils.BussnissLog("用户 " + email + " 已经登陆在其他session,进行重新登陆 " + loginSession);
                 }
-                String uid=null;
+                String uid = null;
                 try {
                     uid = AllDao.getInstance().getSessionDao().getUid(sessionID);
+                } catch (Exception e) {
+                    LogUtils.BussnissLogError("login error", e);
                 }
-                catch (Exception e)
-                {
-                    LogUtils.BussnissLogError("login error",e);
-                }
-                if(!user.getUid().equals(uid))
-                {
-                    if(!RedisUtil.setUserOnLine(user, sessionID)){
+                if (!user.getUid().equals(uid)) {
+                    if (!RedisUtil.setUserOnLine(user, sessionID)) {
                         view.viewString(ParamUtils.errorParam("登陆失败"), response);
                         return;
                     }
@@ -90,7 +87,7 @@ public class UserController {
             LogUtils.BussnissLogError("出现异常", e);
             resultStr = ParamUtils.errorParam("出现异常");
         }
-        LogUtils.BussnissLog("登录get 耗时" + (System.currentTimeMillis()-start) + "ms");
+        LogUtils.BussnissLog("登录get 耗时" + (System.currentTimeMillis() - start) + "ms");
         view.viewString(resultStr, response);
     }
 
@@ -99,7 +96,7 @@ public class UserController {
         Long start = System.currentTimeMillis();
         String resultStr = null;
         try {
-            User user = (User)paramRe.getAttribute("currentUser");
+            User user = (User) paramRe.getAttribute("currentUser");
             user.setPower(null);
             user.setGroups(new ArrayList<Group>(0));
             ResultBean resultBean = new ResultBean();
@@ -113,13 +110,16 @@ public class UserController {
         logger.info("getUserInfo 耗时: " + (System.currentTimeMillis() - start) + "ms");
         view.viewString(resultStr, response);
     }
+
     @RequestMapping(value = "/UpdateInfo", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public @ResponseBody String postUpdate(HttpServletRequest paramRe) {
+    public
+    @ResponseBody
+    String postUpdate(HttpServletRequest paramRe) {
         Long start = System.currentTimeMillis();
         String resultStr = null;
         try {
-            User user = (User)paramRe.getAttribute("currentUser");
-            if (user == null ) {
+            User user = (User) paramRe.getAttribute("currentUser");
+            if (user == null) {
                 logger.error("paramRe里面无currentUser");
                 return ParamUtils.errorSessionLosParam();
             }
@@ -134,11 +134,11 @@ public class UserController {
             } catch (Exception e) {
                 return ParamUtils.errorParam("缺少uid");
             }
-            ResultBean resultBean =  processor.update(param);
+            ResultBean resultBean = processor.update(param);
             UserProcessor.currentUpdate(user.getUid(), paramRe.getSession(false).getId());
             resultStr = gson.toJson(resultBean);
         } catch (Exception e) {
-            logger.error("",e);
+            logger.error("", e);
             resultStr = ParamUtils.errorParam("出现异常");
         }
         logger.info("用户更新个人信息 post 耗时" + (System.currentTimeMillis() - start) + "ms");
@@ -146,15 +146,16 @@ public class UserController {
     }
 
 
-
     @RequestMapping(value = "/UpdatePWD", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public @ResponseBody String UpdatePWD(HttpServletRequest paramRe){
+    public
+    @ResponseBody
+    String UpdatePWD(HttpServletRequest paramRe) {
         Long start = System.currentTimeMillis();
         String resultStr = null;
         try {
             String param = ParamUtils.getParam(paramRe);
-            logger.info("UpdatePWD param="+param);
-            resultStr =  processor.updatePWD(param);
+            logger.info("UpdatePWD param=" + param);
+            resultStr = processor.updatePWD(param);
         } catch (Exception e) {
             logger.error("", e);
             resultStr = ParamUtils.errorParam("出现异常");
@@ -165,15 +166,17 @@ public class UserController {
     }
 
 
-    @RequestMapping(value = "/SendEmailForChangePWD",method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public @ResponseBody String postChangePWD(HttpServletRequest paramRe) {
+    @RequestMapping(value = "/SendEmailForChangePWD", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public
+    @ResponseBody
+    String postChangePWD(HttpServletRequest paramRe) {
         Long start = System.currentTimeMillis();
         String resultStr = null;
         try {
             String param = ParamUtils.getParam(paramRe);
             JsonObject paramObj = (JsonObject) jsonParser.parse(param);
             logger.info("SendEmailForChangePWD param=" + param);
-            resultStr =  processor.changePwdSender(paramObj);
+            resultStr = processor.changePwdSender(paramObj);
         } catch (Exception e) {
             logger.error("", e);
             resultStr = ParamUtils.errorParam("出现异常");
@@ -181,13 +184,16 @@ public class UserController {
         logger.info("发送修改密码邮件 post 耗时" + (System.currentTimeMillis() - start) + "ms");
         return resultStr;
     }
+
     @RequestMapping(value = "/ExistEmail", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
-    public @ResponseBody String postExistEmail(@RequestParam("param")String param) {
+    public
+    @ResponseBody
+    String postExistEmail(@RequestParam("param") String param) {
         Long start = System.currentTimeMillis();
         String resultStr = null;
         try {
             JsonObject paramObj = (JsonObject) jsonParser.parse(param);
-            resultStr =  processor.existEmail(paramObj);
+            resultStr = processor.existEmail(paramObj);
         } catch (Exception e) {
             logger.error("", e);
             resultStr = ParamUtils.errorParam("出现异常");
@@ -197,11 +203,13 @@ public class UserController {
     }
 
     @RequestMapping(value = "/CRFList", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
-    public @ResponseBody String CRFList(HttpServletRequest paramRe) {
+    public
+    @ResponseBody
+    String CRFList(HttpServletRequest paramRe) {
         Long start = System.currentTimeMillis();
         String resultStr = null;
         try {
-            User user = (User)paramRe.getAttribute("currentUser");
+            User user = (User) paramRe.getAttribute("currentUser");
             return processor.CRFList(user);
         } catch (Exception e) {
             logger.error("", e);
@@ -213,12 +221,14 @@ public class UserController {
     }
 
     @RequestMapping(value = "/VitaBoardConfigSave", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public @ResponseBody String VitaBoardConfigSave(HttpServletRequest paramRe) {
+    public
+    @ResponseBody
+    String VitaBoardConfigSave(HttpServletRequest paramRe) {
         Long start = System.currentTimeMillis();
         String resultStr = null;
         try {
             String param = ParamUtils.getParam(paramRe);
-            User user = (User)paramRe.getAttribute("currentUser");
+            User user = (User) paramRe.getAttribute("currentUser");
             return processor.vitaBoardConfigSave(user.getUid(), param);
         } catch (Exception e) {
             logger.error("", e);
@@ -229,56 +239,65 @@ public class UserController {
         return resultStr;
     }
 
-    @RequestMapping(value="/VitaBoardConfig",method= RequestMethod.GET,produces = "application/json;charset=UTF-8")
-    public @ResponseBody String VitaBoardConfig(HttpServletRequest paramRe) {
+    @RequestMapping(value = "/VitaBoardConfig", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    public
+    @ResponseBody
+    String VitaBoardConfig(HttpServletRequest paramRe) {
         Long start = System.currentTimeMillis();
         String resultStr = null;
         try {
-            User user = (User)paramRe.getAttribute("currentUser");
+            User user = (User) paramRe.getAttribute("currentUser");
             return processor.VitaBoardConfig(user.getUid());
         } catch (Exception e) {
             logger.error("", e);
             resultStr = ParamUtils.errorParam("出现异常");
         }
-        logger.info("CRFList 返回="+resultStr);
-        logger.info("用户可访问crf列表 post 耗时"+(System.currentTimeMillis()-start) +"ms");
+        logger.info("CRFList 返回=" + resultStr);
+        logger.info("用户可访问crf列表 post 耗时" + (System.currentTimeMillis() - start) + "ms");
         return resultStr;
     }
 
-    @RequestMapping(value="/LabTransformCrfId",method= RequestMethod.GET,produces = "application/json;charset=UTF-8")
-    public @ResponseBody String LabTransformCrfId(HttpServletRequest paramRe) {
+    @RequestMapping(value = "/LabTransformCrfId", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    public
+    @ResponseBody
+    String LabTransformCrfId(HttpServletRequest paramRe) {
         Long start = System.currentTimeMillis();
         String resultStr = null;
         try {
-            User user = (User)paramRe.getAttribute("currentUser");
+            User user = (User) paramRe.getAttribute("currentUser");
             String param = ParamUtils.getParam(paramRe);
-            return processor.labTransformCrfId(user,param);
+            return processor.labTransformCrfId(user, param);
         } catch (Exception e) {
-            logger.error("",e);
+            logger.error("", e);
             resultStr = ParamUtils.errorParam("出现异常");
         }
-        logger.info("科室转化成病种id 返回="+resultStr);
-        logger.info("科室转化成病种id get 耗时"+(System.currentTimeMillis()-start) +"ms");
+        logger.info("科室转化成病种id 返回=" + resultStr);
+        logger.info("科室转化成病种id get 耗时" + (System.currentTimeMillis() - start) + "ms");
         return resultStr;
     }
 
-    @RequestMapping(value="/SetRedis",method= RequestMethod.GET,produces = "application/json;charset=UTF-8")
-    public @ResponseBody String OffLineUser(HttpServletRequest paramRe) {
+    @RequestMapping(value = "/SetRedis", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    public
+    @ResponseBody
+    String OffLineUser(HttpServletRequest paramRe) {
         Long start = System.currentTimeMillis();
         String resultStr = null;
         try {
             String param = ParamUtils.getParam(paramRe);
             return processor.setRedis(param);
         } catch (Exception e) {
-            logger.error("",e);
+            logger.error("", e);
             resultStr = ParamUtils.errorParam("出现异常");
         }
-        logger.info("清空缓存中用户信息 返回="+resultStr);
-        logger.info(" get 耗时"+(System.currentTimeMillis()-start) +"ms");
+        logger.info("清空缓存中用户信息 返回=" + resultStr);
+        logger.info(" get 耗时" + (System.currentTimeMillis() - start) + "ms");
         return resultStr;
     }
-    @RequestMapping(value="/logout",method= RequestMethod.GET,produces = "application/json;charset=UTF-8")
-    public @ResponseBody String logout(HttpServletRequest paramRe) {
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    public
+    @ResponseBody
+    String logout(HttpServletRequest paramRe) {
         try {
             String sessionId = paramRe.getSession(false).getId();
             RedisUtil.userLogout(sessionId);
@@ -291,19 +310,21 @@ public class UserController {
         }
     }
 
-    @RequestMapping(value="/IsInnerNet",method= RequestMethod.GET,produces = "application/json;charset=UTF-8")
-    public @ResponseBody String IsInnerNet(HttpServletRequest paramRe){
-        Object xRealIpobj=null;
+    @RequestMapping(value = "/IsInnerNet", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    public
+    @ResponseBody
+    String IsInnerNet(HttpServletRequest paramRe) {
+        Object xRealIpobj = null;
         try {
             xRealIpobj = SpringContextUtil.getBean("xrealip");
             if (xRealIpobj == null) {
                 return ParamUtils.errorParam("无内外网限制");
             }
         } catch (Exception e) {
-            logger.error("error 无内外网限制 ",e);
+            logger.error("error 无内外网限制 ", e);
             return ParamUtils.errorParam("无内外网限制");
         }
-        XRealIp xRealIp = (XRealIp)xRealIpobj;
+        XRealIp xRealIp = (XRealIp) xRealIpobj;
         String value = paramRe.getHeader(xRealIp.getKey());
         if (StringUtils.isEmpty(value)) {
             return ParamUtils.errorParam("无对应key");
@@ -313,26 +334,28 @@ public class UserController {
         JsonObject json = new JsonObject();
         logger.info("ip: " + value);
         if (value.equals(xRealIp.getValue())) {
-            json.addProperty("inner",true);
+            json.addProperty("inner", true);
             resultBean.setMsg("内网");
         } else {
-            json.addProperty("inner",false);
+            json.addProperty("inner", false);
             resultBean.setMsg("外网");
             resultBean.setData(json);
         }
         return gson.toJson(resultBean);
     }
+
     /**
-     *
      * 科室权限校验
-     * */
-    @RequestMapping(value="/checkUserRole",method= RequestMethod.POST,produces = "application/json;charset=UTF-8")
-    public @ResponseBody String checkUserRole(HttpServletRequest paramRe) {
+     */
+    @RequestMapping(value = "/checkUserRole", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public
+    @ResponseBody
+    String checkUserRole(HttpServletRequest paramRe) {
         try {
             JsonObject paramJson = jsonParser.parse(ParamUtils.getParam(paramRe)).getAsJsonObject();
-            String  dept= paramJson.get("dept").getAsString();
-            if(StringUtils.isEmpty(dept)) return  ParamUtils.errorParam("空科室");
-            User user = (User)paramRe.getAttribute("currentUser");
+            String dept = paramJson.get("dept").getAsString();
+            if (StringUtils.isEmpty(dept)) return ParamUtils.errorParam("空科室");
+            User user = (User) paramRe.getAttribute("currentUser");
             Power power = user.getPower();
             Set<Resource> list = power.getHas_search();
             if (list == null || list.size() == 0) {
@@ -340,7 +363,7 @@ public class UserController {
             }
             boolean find = false;
             String orgID = user.getOrgID();
-            for (Resource resource: list) {
+            for (Resource resource : list) {
                 if (dept.equals(resource.getSlab_name()) && "有".equals(resource.getHas_search())) {
                     find = true;
                     break;
@@ -361,7 +384,7 @@ public class UserController {
             }
             ResultBean resultBean = new ResultBean();
             JsonObject json = new JsonObject();
-            json.addProperty("check",true);
+            json.addProperty("check", true);
             json.add("depts", gson.toJsonTree(depts));
             resultBean.setData(json);
             return gson.toJson(resultBean);
@@ -369,14 +392,17 @@ public class UserController {
             return ParamUtils.errorParam("异常error");
         }
     }
-    @RequestMapping(value="/getUser",method= RequestMethod.GET,produces = "application/json;charset=UTF-8")
-    public @ResponseBody String getUser(HttpServletRequest paramRe) {
+
+    @RequestMapping(value = "/getUser", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    public
+    @ResponseBody
+    String getUser(HttpServletRequest paramRe) {
         HttpSession session = paramRe.getSession(false);
         String sessionID = session.getId();
-        String uid=RedisUtil.getValue(sessionID);
+        String uid = RedisUtil.getValue(sessionID);
         logger.info("getUser sessionID = " + sessionID + " uid = " + uid);
         User user = UserProcessor.getUserByUidFromRedis(uid);
-        ResultBean bean=new ResultBean();
+        ResultBean bean = new ResultBean();
         bean.setData(user);
         return gson.toJson(bean);
     }
