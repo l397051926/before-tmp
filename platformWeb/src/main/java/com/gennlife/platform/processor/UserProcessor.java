e5716c112250deb1940c278deb3e91282c5fba15
chenjinfeng <chenjinfeng@gennlife.com>
2017-04-12 14:46:31 +0800

    bug fix

--- platformWeb/src/main/java/com/gennlife/platform/processor/UserProcessor.java
+++ platformWeb/src/main/java/com/gennlife/platform/processor/UserProcessor.java
@@ -216,7 +216,7 @@ public class UserProcessor {
             }
             list.clear();
             list.add(resultGroup);
-            if (platformEnvrionment.equals("false")) { // Group清空
+            if (platformEnvrionment!=null&&platformEnvrionment.equals("false")) { // Group清空
                 user.setGroups(null);
             } else {
                 user.setGroups(list);
@@ -246,6 +246,7 @@ public class UserProcessor {
                 confMap.put("uid", user.getUid());
                 List<Role> rolesList = AllDao.getInstance().getSyRoleDao().getRoles(confMap);
                 //转化本科室信息
+                transformRole(user,rolesList);
                 user.setRoles(rolesList);
                 return user;
             }
@@ -434,48 +435,48 @@ public class UserProcessor {
     public static Power addResourceInGroupToPower(Power power, Resource resource, Group group) {
         if ("有".equals(resource.getHas_search()) && "有".equals(group.getHas_search())) {
             if (!isExistResource(power.getHas_search(), resource)) {
-                power.getHas_search().add(resource);
+                power.addInHasSearch(resource);
             }
 
         }
         if ("有".equals(resource.getHas_searchExport()) && "有".equals(group.getHas_searchExport())) {
             if (!isExistResource(power.getHas_searchExport(), resource)) {
-                power.getHas_searchExport().add(resource);
+                power.addInHasSearchExport(resource);
             }
 
         }
         if ("有".equals(resource.getHas_traceCRF()) && "有".equals(group.getHas_traceCRF())) {
             if (!isExistResource(power.getHas_traceCRF(), resource)) {
-                power.getHas_traceCRF().add(resource);
+                power.addInHasTraceCRF(resource);
             }
 
         }
         if ("有".equals(resource.getHas_addCRF()) && "有".equals(group.getHas_addCRF())) {
             if (!isExistResource(power.getHas_addCRF(), resource)) {
-                power.getHas_addCRF().add(resource);
+                power.addInHasAddCRF(resource);
             }
         }
         if ("有".equals(resource.getHas_addBatchCRF()) && "有".equals(group.getHas_addBatchCRF())) {
             if (!isExistResource(power.getHas_addBatchCRF(), resource)) {
-                power.getHas_addBatchCRF().add(resource);
+                power.addInHasAddBatchCRF(resource);
             }
 
         }
         if ("有".equals(resource.getHas_editCRF()) && "有".equals(group.getHas_editCRF())) {
             if (!isExistResource(power.getHas_editCRF(), resource)) {
-                power.getHas_editCRF().add(resource);
+                power.addInHasEditCRF(resource);
             }
 
         }
         if ("有".equals(resource.getHas_deleteCRF()) && "有".equals(group.getHas_deleteCRF())) {
             if (!isExistResource(power.getHas_deleteCRF(), resource)) {
-                power.getHas_deleteCRF().add(resource);
+                power.addInHasDeleteCRF(resource);
             }
 
         }
         if ("有".equals(resource.getHas_browseDetail()) && "有".equals(group.getHas_browseDetail())) {
             if (!isExistResource(power.getHas_browseDetail(), resource)) {
-                power.getHas_browseDetail().add(resource);
+                power.addInHasBrowseDetail(resource);
             }
 
         }
