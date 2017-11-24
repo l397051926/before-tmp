6235f846e380a3f1dd6a42075278c923c8a95867
luoxupan <ygxqqx@163.com>
2017-11-24 11:55:16 +0800

    fix

--- platformWeb/src/main/java/com/gennlife/platform/processor/SampleProcessor.java
+++ platformWeb/src/main/java/com/gennlife/platform/processor/SampleProcessor.java
@@ -486,9 +486,9 @@ public class SampleProcessor {
                  String data_02 = HttpRequestUtils.httpPost(url, GsonUtil.getGson().toJson(queryNew));
 
                  JsonObject hits_01 = jsonParser.parse(data_01).getAsJsonObject().get("data").getAsJsonObject().get("hits").getAsJsonObject();
-                 logger.info("data_01=" + gson.toJson(data_01));
+                 logger.info("data_01=" + data_01);
                  JsonObject hits_02 = jsonParser.parse(data_02).getAsJsonObject().get("data").getAsJsonObject().get("hits").getAsJsonObject();
-                 logger.info("data_02=" + gson.toJson(data_02));
+                 logger.info("data_02=" + data_02);
                  logger.info("total========= " + hits_01.get("total").getAsInt() + " ======== " + hits_02.get("total").getAsInt());
                  int count = hits_01.get("total").getAsInt() - hits_02.get("total").getAsInt();
                  if (count > 0) {
