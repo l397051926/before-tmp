eb41c7a99b7974b838b7bb2ab9ca64862643adbc
luoxupan <ygxqqx@163.com>
2018-02-06 21:19:37 +0800

    fix code

--- platformWeb/src/main/resources/shell/old.py
+++ platformWeb/src/main/resources/shell/old.py
@@ -21,7 +21,7 @@ def sortItem(itemArray,allSortIndexName):
     return newArray
 
 def process():
-    data = xlrd.open_workbook('/Users/luoxupan/Downloads/病人维度临床数据字段配置_for烟台毓璜顶医院版本V3.1.1.2.xlsx')
+    data = xlrd.open_workbook('/Users/luoxupan/Downloads/病人维度临床数据字段配置_V3.1.6.2.xlsx')
     allItemList = OrderedDict()
     allItemListCopy = OrderedDict()
     sheet = data.sheets()[0]
