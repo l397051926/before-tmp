ac5c039350a98acee369c78b60466ed6757b1828
liumingxin@gennlife.com <18645484821@163.com>
2018-06-08 16:01:43 +0800

    修改配置文件

--- platformWeb/src/test/java/com/gennlife/platform/ConditionTest.java
+++ platformWeb/src/test/java/com/gennlife/platform/ConditionTest.java
@@ -2,13 +2,16 @@ package com.gennlife.platform;
 
 import com.gennlife.platform.ReadConfig.LoadCrfCondition;
 import com.gennlife.platform.service.CrfConditionService;
+import com.google.gson.Gson;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
 import org.junit.Test;
+import org.springframework.util.StringUtils;
 
 import java.util.Map;
 
 /**
- * @author liumingxin
+ * @author liumingxin---09
  * @create 2018 17 20:26
  * @desc
  **/
@@ -26,6 +29,11 @@ public class ConditionTest {
         System.out.println("load Crf Condition Test");
         JsonObject jsonObject = LoadCrfCondition.getCrfSearch("CVD");
         System.out.println("demo");
+    }
+    @Test
+    public void jsonObject(){
+        JsonObject jsonObject = new JsonObject();
+        System.out.println(jsonObject.entrySet().size()==0);
     }
 
 }
