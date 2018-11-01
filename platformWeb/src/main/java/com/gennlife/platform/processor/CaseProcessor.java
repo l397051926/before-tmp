d20c60d0336987ca988e06ca74358af937af8d61
liumingxin@gennlife.com <18645484821@163.com>
2018-11-02 07:30:07 +0800

    增加 emr 支持rws 配置文件

--- platformWeb/src/main/java/com/gennlife/platform/processor/CaseProcessor.java
+++ platformWeb/src/main/java/com/gennlife/platform/processor/CaseProcessor.java
@@ -274,11 +274,12 @@ public class CaseProcessor {
             resultBean.setData(allNew);
         } else if ("7".equals(status)) {//rws 检索结果列表
             JsonObject all = new JsonObject();
-            if (emr_id.equals(crf_id) ||"EMR".equals(crf_id)) {
-                all = ConfigurationService.getAdvancedSearch(crf_id);
-            } else {
-                all = ReadConditionByRedis.getCrfSearch(crf_id);
-            }
+//            if (emr_id.equals(crf_id) ||"EMR".equals(crf_id)) {
+//                all = ConfigurationService.getAdvancedSearch(crf_id);
+//            } else {
+//                all = ReadConditionByRedis.getCrfSearch(crf_id);
+//            }
+            all = ReadConditionByRedis.getCrfSearch(crf_id);
             JsonObject allNew = new JsonObject();
             for (Map.Entry<String, JsonElement> obj : all.entrySet()) {
                 String groupName = obj.getKey();
@@ -341,11 +342,12 @@ public class CaseProcessor {
             resultBean.setData(allNew);
         } else if ("8".equals(status)) {//高级搜索,所有属性,带有搜索功能
             JsonObject all = new JsonObject();
-            if (emr_id.equals(crf_id) ||"EMR".equals(crf_id)) {
-                all = ConfigurationService.getAdvancedSearch(crf_id);
-            } else {
-                all = ReadConditionByRedis.getCrfSearch(crf_id);
-            }
+//            if (emr_id.equals(crf_id) ||"EMR".equals(crf_id)) {
+//                all = ConfigurationService.getAdvancedSearch(crf_id);
+//            } else {
+//                all = ReadConditionByRedis.getCrfSearch(crf_id);
+//            }
+            all = ReadConditionByRedis.getCrfSearch(crf_id);
             JsonObject allNew = new JsonObject();
             for (Map.Entry<String, JsonElement> obj : all.entrySet()) {
                 String groupName = obj.getKey();
@@ -983,11 +985,12 @@ public class CaseProcessor {
 
         if ("0".equals(status)) {//高级搜索,所有属性,带有搜索功能
             JsonObject all = new JsonObject();
-            if (emr_id.equals(crf_id) ||"EMR".equals(crf_id)) {
-                all = ConfigurationService.getAdvancedSearch(crf_id);
-            } else {
-                all = ReadConditionByRedis.getCrfSearch(crf_id);
-            }
+//            if (emr_id.equals(crf_id) ||"EMR".equals(crf_id)) {
+//                all = ConfigurationService.getAdvancedSearch(crf_id);
+//            } else {
+//                all = ReadConditionByRedis.getCrfSearch(crf_id);
+//            }
+            all = ReadConditionByRedis.getCrfSearch(crf_id);
             JsonObject allNew = new JsonObject();
             for (Map.Entry<String, JsonElement> obj : all.entrySet()) {
                 String groupName = obj.getKey();
