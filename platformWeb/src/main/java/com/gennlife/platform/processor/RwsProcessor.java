da9209648192ee1751fcb7a4138b5881121be17e
liumingxin@gennlife.com <18645484821@163.com>
2018-12-01 11:16:31 +0800

    增加项目统计接口

--- platformWeb/src/main/java/com/gennlife/platform/processor/RwsProcessor.java
+++ platformWeb/src/main/java/com/gennlife/platform/processor/RwsProcessor.java
@@ -759,4 +759,15 @@ public class RwsProcessor {
             return ParamUtils.errorParam("请求发生异常");
         }
     }
+
+    public String projectAggregation(JsonObject paramObj) {
+        try {
+            String url = ConfigurationService.getUrlBean().getProjectAggregation();
+            String result = HttpRequestUtils.httpPost(url, gson.toJson(paramObj));
+            return result;
+        } catch (Exception e) {
+            logger.error("获取项目统计列表 ", e);
+            return ParamUtils.errorParam("请求发生异常");
+        }
+    }
 }
