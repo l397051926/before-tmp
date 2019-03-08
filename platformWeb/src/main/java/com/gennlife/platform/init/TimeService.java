e2132bc76a89f78c5750689123a060f0f92746e7
liumingxin@gennlife.com <18645484821@163.com>
2019-03-08 22:21:47 +0800

    增加 稳定每次查询 select 1 数据功能

---
+++ platformWeb/src/main/java/com/gennlife/platform/init/TimeService.java
@@ -0,0 +1,31 @@
+package com.gennlife.platform.init;
+
+import com.gennlife.platform.dao.AllDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author lmx
+ * @create 2019 08 22:13
+ * @desc
+ **/
+@Component
+@Configuration
+@EnableScheduling
+public class TimeService {
+    private Logger LOG = LoggerFactory.getLogger(TimeService.class);
+
+
+    /**
+     * 定时清理_tmp 数据的数据 半夜十二点 清理
+     */
+    @Scheduled(cron = "0/3 * * * * ?")
+    public void reSubmitTask(){
+        AllDao.getInstance().getOrgDao().seleteA();
+        LOG.info("------执行了 稳定查询数据库 每 5秒");
+    }
+}
