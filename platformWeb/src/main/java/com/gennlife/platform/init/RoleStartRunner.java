39cd820d8634ae032d18e2aa2ddf403946d4836f
liumingxin@gennlife.com <18645484821@163.com>
2019-04-05 11:19:21 +0800

    调整配置文件

--- platformWeb/src/main/java/com/gennlife/platform/init/RoleStartRunner.java
+++ platformWeb/src/main/java/com/gennlife/platform/init/RoleStartRunner.java
@@ -2,10 +2,14 @@ package com.gennlife.platform.init;
 
 import com.gennlife.platform.ReadConfig.ReadConditionByRedis;
 import com.gennlife.platform.processor.LaboratoryProcessor;
+import com.gennlife.platform.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author liumingxin
@@ -22,6 +26,12 @@ public class RoleStartRunner implements CommandLineRunner {
         System.out.println("-------------------------------创建全员角色表------------------------------------------------");
         //读 crfMAPPING
         ReadConditionByRedis.loadCrfMapping();
+        List<String> keys = new ArrayList<>();
+        keys.add("detail_schema");
+        keys.add("etl_data_count");
+        for (String key : keys){
+            RedisUtil.deleteKey(key);
+        }
         processor.addAllRole();
     }
 }
